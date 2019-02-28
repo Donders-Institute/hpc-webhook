7adadfdeace460a2a9e2daab9d85b7fccfff09bd
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2019-02-28 13:07:27 +0100

    Improve waiting for db in client and server Docker files

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -21,6 +21,7 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+
 	db, err := server.InitDB(psqlInfo)
 	if err != nil {
 		panic(err)
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,6 @@
 echo Change Windows to Unix line endings
 sed -i 's/\r//g' ../init/01-initialize-database.sh
 sed -i 's/\r//g' wait-for-it.sh
+
 docker-compose -f ../docker-compose.yml build --no-cache
 docker-compose -f ../docker-compose.yml up
