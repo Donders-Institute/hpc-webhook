66508442d2758e1ea7556eef83afbbcb6797431a
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2019-03-01 12:06:17 +0100

    Use postgres serial ID for autoincrement, etc. in qaas table

--- init/01-initialize-database.sh
+++ init/01-initialize-database.sh
@@ -4,9 +4,8 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DATABASE" <<-EOSQL
     DROP TABLE IF EXISTS qaas;
     CREATE TABLE qaas(
-        id         INT              NOT NULL,
+        id         SERIAL PRIMARY KEY,
         hash       CHAR (64)        NOT NULL,
         username   VARCHAR (32)     NOT NULL,
-        PRIMARY KEY (ID)
     );
 EOSQL
