7152fb2fdd9739e7f5f3cd4525dd039a0d443357
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2019-03-01 12:13:56 +0100

    Improve db init scripts

--- test/init/01-initialize-database.sh
+++ test/init/01-initialize-database.sh
@@ -6,6 +6,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DATABASE
     CREATE TABLE qaas(
         id         SERIAL PRIMARY KEY,
         hash       CHAR (64)        NOT NULL,
-        username   VARCHAR (32)     NOT NULL,
-    );
+        username   VARCHAR (32)     NOT NULL);
 EOSQL
