a200e134b44e3c0a021fbfb8f8695d7e4aeff69f
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2019-03-08 10:52:42 +0100

    Started on add, list and delete configuration routes

--- internal/server/configuration.go
+++ internal/server/configuration.go
@@ -15,12 +15,21 @@ type ConfigurationRequest struct {
 	Username  string `json:"username"`
 }
 
-// ConfigurationResponse contains the complet webhook payload URL
+// ConfigurationResponse contains the complete webhook payload URL
 type ConfigurationResponse struct {
 	Webhook string `json:"webhook"`
 }
 
-// Parse reads and verifies the add hook in an inbound request.
+// ConfigurationListResponse contains thelist of regstered webhooks for a certain user
+type ConfigurationListResponse struct {
+	Webhooks string `json:"webhooks"`
+}
+
+// ConfigurationDeleteResponse contains the webhook that has been deleted
+type ConfigurationDeleteResponse struct {
+	Webhook string `json:"webhook"`
+}
+
 func parseConfigurationRequest(req *http.Request) (ConfigurationRequest, error) {
 	var configuration ConfigurationRequest
 	var err error
@@ -51,9 +60,87 @@ func parseConfigurationRequest(req *http.Request) (ConfigurationRequest, error) 
 	return configuration, err
 }
 
+func parseConfigurationListRequest(req *http.Request) (ConfigurationRequest, error) {
+	var configuration ConfigurationRequest
+	var err error
+
+	// Check method
+	if !strings.EqualFold(req.Method, "GET") {
+		return configuration, fmt.Errorf("invalid method '%s'", req.Method)
+	}
+
+	// Check the URL path
+	if !isValidConfigurationURLPath(req.URL.Path) {
+		return configuration, fmt.Errorf("invalid URL path '%s'", req.URL.Path)
+	}
+
+	// Obtain the configuration
+	decoder := json.NewDecoder(req.Body)
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		return configuration, errors.New("invalid JSON body")
+	}
+
+	// Validate the configuration
+	err = validateConfigurationRequest(configuration)
+	if err != nil {
+		return configuration, err
+	}
+
+	return configuration, err
+}
+
+func parseConfigurationDeleteRequest(req *http.Request) (ConfigurationRequest, error) {
+	var configuration ConfigurationRequest
+	var err error
+
+	// Check method
+	if !strings.EqualFold(req.Method, "DELETE") {
+		return configuration, fmt.Errorf("invalid method '%s'", req.Method)
+	}
+
+	// Check the URL path
+	if !isValidConfigurationURLPath(req.URL.Path) {
+		return configuration, fmt.Errorf("invalid URL path '%s'", req.URL.Path)
+	}
+
+	// Obtain the configuration
+	decoder := json.NewDecoder(req.Body)
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		return configuration, errors.New("invalid JSON body")
+	}
+
+	// Validate the configuration
+	err = validateConfigurationRequest(configuration)
+	if err != nil {
+		return configuration, err
+	}
+
+	return configuration, err
+}
+
 // ConfigurationHandler handles a webhook registration HTTP PUT request
 // with the hash and username in its body
 func (a *API) ConfigurationHandler(w http.ResponseWriter, req *http.Request) {
+	switch method := req.Method; method {
+	case "PUT":
+		a.ConfigurationHandlerAdd(w, req)
+	case "GET":
+		a.ConfigurationHandlerList(w, req)
+	case "DELETE":
+		a.ConfigurationHandlerDelete(w, req)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		err := fmt.Errorf("invalid method '%s'", req.Method)
+		fmt.Fprint(w, "Error 404 - Not found: ", err)
+		return
+	}
+}
+
+// ConfigurationHandlerAdd handles a HTTP PUT request
+// to register a certain webhook with hash, groupname, and username in its body
+func (a *API) ConfigurationHandlerAdd(w http.ResponseWriter, req *http.Request) {
 	// Parse and validate the request
 	configuration, err := parseConfigurationRequest(req)
 	if err != nil {
@@ -87,6 +174,82 @@ func (a *API) ConfigurationHandler(w http.ResponseWriter, req *http.Request) {
 		Webhook: webhookPayloadURL,
 	}
 	js, err := json.Marshal(configurationResponse)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Error 404 - Not found: ", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	return
+}
+
+// ConfigurationHandlerList handles a HTTP GET request
+// to obtain all webhooks for a certain user
+func (a *API) ConfigurationHandlerList(w http.ResponseWriter, req *http.Request) {
+	// Parse and validate the request
+	configuration, err := parseConfigurationListRequest(req)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Println(err)
+		fmt.Fprint(w, "Error 404 - Not found: ", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", configuration)
+	// // Add a row in the database
+	// err = getRows(a.DB, configuration.Groupname, configuration.Username)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusNotFound)
+	// 	fmt.Println(err)
+	// 	fmt.Fprint(w, "Error 404 - Not found: ", err)
+	// 	return
+	// }
+
+	// Succes
+	configurationListResponse := ConfigurationListResponse{
+		Webhooks: "",
+	}
+	js, err := json.Marshal(configurationListResponse)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "Error 404 - Not found: ", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	return
+}
+
+// ConfigurationHandlerDelete handles a HTTP DELETE request
+// to delete a certain webhook for a certain user
+func (a *API) ConfigurationHandlerDelete(w http.ResponseWriter, req *http.Request) {
+	// Parse and validate the request
+	configuration, err := parseConfigurationDeleteRequest(req)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Println(err)
+		fmt.Fprint(w, "Error 404 - Not found: ", err)
+		return
+	}
+
+	fmt.Printf("%+v\n", configuration)
+	// // Delete a row in the database
+	// err = deleteRow(a.DB, configuration.Hash, configuration.Groupname, configuration.Username)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusNotFound)
+	// 	fmt.Println(err)
+	// 	fmt.Fprint(w, "Error 404 - Not found: ", err)
+	// 	return
+	// }
+
+	// Succes
+	configurationDeleteResponse := ConfigurationDeleteResponse{
+		Webhook: configuration.Hash,
+	}
+	js, err := json.Marshal(configurationDeleteResponse)
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "Error 404 - Not found: ", err)
