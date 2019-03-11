2274560f56c5dba56968767fdd8c567dd4eb23e0
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2019-03-11 20:30:47 +0100

    Update initialize db scripts

--- init/01-initialize-database.sh
+++ init/01-initialize-database.sh
@@ -7,7 +7,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DATABASE
         hash        CHAR (36) UNIQUE NOT NULL,
         groupname   VARCHAR (32) NOT NULL,
         username    VARCHAR (32) NOT NULL,
-        script      VARCHAR (255) NOT NULL,
         description VARCHAR (255),
         created     TIMESTAMP NOT NULL);
 EOSQL
