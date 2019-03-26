d18acee2c894f8ff3a468d75bf73b4b9acab999d
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2019-03-26 10:37:17 +0100

    Fix integration test

--- internal/server/configuration_integration_test.go
+++ internal/server/configuration_integration_test.go
@@ -261,7 +261,7 @@ func TestConfigurationInfoHandler(t *testing.T) {
 				"Content-Type": "application/json; charset=utf-8",
 			},
 			expectedStatus: 200,
-			expectedString: `{"webhook":{"hash":"550e8400-e29b-41d4-a716-446655440001","groupname":"groupname","username":"username","description":"description","created":"2019-03-11T19:44:44+01:00","url":"https://hpc-webhook.dccn.nl:443/550e8400-e29b-41d4-a716-446655440001"}}`,
+			expectedString: `{"webhook":{"hash":"550e8400-e29b-41d4-a716-446655440001","groupname":"groupname","username":"username","description":"description","created":"2019-03-11T19:44:44+01:00","url":"https://hpc-webhook.dccn.nl:443/webhook/550e8400-e29b-41d4-a716-446655440001"}}`,
 			expectedResult: true, // No error
 		},
 		{
@@ -426,7 +426,7 @@ func TestConfigurationListHandler(t *testing.T) {
 				"Content-Type": "application/json; charset=utf-8",
 			},
 			expectedStatus: 200,
-			expectedString: `{"webhooks":[{"hash":"550e8400-e29b-41d4-a716-446655440001","groupname":"groupname","username":"username","description":"","created":"2019-03-11T19:44:44+01:00","url":"https://hpc-webhook.dccn.nl:443/550e8400-e29b-41d4-a716-446655440001"},{"hash":"550e8400-e29b-41d4-a716-446655440002","groupname":"groupname","username":"username","description":"","created":"2019-03-11T19:45:44+01:00","url":"https://hpc-webhook.dccn.nl:443/550e8400-e29b-41d4-a716-446655440002"}]}`,
+			expectedString: `{"webhooks":[{"hash":"550e8400-e29b-41d4-a716-446655440001","groupname":"groupname","username":"username","description":"","created":"2019-03-11T19:44:44+01:00","url":"https://hpc-webhook.dccn.nl:443/webhook/550e8400-e29b-41d4-a716-446655440001"},{"hash":"550e8400-e29b-41d4-a716-446655440002","groupname":"groupname","username":"username","description":"","created":"2019-03-11T19:45:44+01:00","url":"https://hpc-webhook.dccn.nl:443/webhook/550e8400-e29b-41d4-a716-446655440002"}]}`,
 			expectedResult: true, // No error
 		},
 		{
